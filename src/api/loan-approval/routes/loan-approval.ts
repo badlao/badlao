67f924ae950bb15cd915214a4fc612980606dc05
nazmul-prince <nazmulfarhan@gmail.com>
2025-09-26 17:17:14 +0600

    policy enforcement tested

--- src/api/loan-approval/routes/loan-approval.ts
+++ src/api/loan-approval/routes/loan-approval.ts
@@ -1,28 +1,24 @@
-// module.exports = {
-//   routes: [
-//     {
-//       method: 'POST',
-//       path: '/loan-applications/:id/approve',
-//       handler: 'loan-approval.approve',
-//       config: {
-//         policies: [],
-//         auth: {
-//           scope: ['plugin::users-permissions.custom-approve-loan']
-//         }
-//       },
-//     }
-//   ]
-// };
 module.exports = {
-  routes: [
+    routes: [
     {
       method: 'POST',
       path: '/loan-applications/:id/forward-with-status',
       handler: 'loan-approval.forwardLoanStatus',
       config: {
-        policies: [],
-        
+        policies: [
+          {
+            name: 'global::loan-forward-guard',
+            config: {
+              key: 'status', // the body key to read
+              map: {
+                APPROVED: ['manager'],       // only Managers can approve
+                DISBURSED: ['field-officer'] // only Field Officers can disburse
+              },
+            },
+          },
+        ],
+        middlewares: [],
       },
-    }
-  ]
+    },
+  ],
 };
---
+++ src/policies/loan-forward-guard.ts
@@ -0,0 +1,22 @@
+export default async (policyContext, config, { strapi }) => {
+  // Add your own logic here.
+  // For example:
+  const { user } = policyContext.state;
+  if (!user) {
+    return false;
+  }
+
+  const key = config.key || 'status';
+  const requested = policyContext.request.body[key];
+  if(!requested){
+    return false;
+  }
+  const roleName = user.role?.name?.toLowerCase();
+  if(!roleName){
+    return false;
+  }
+  const roleMap = config?.map ?? [];
+  const allowedRoles = (roleMap[requested] ??  []).map(r => r.toLowerCase());
+
+  return allowedRoles.includes(roleName);
+}
