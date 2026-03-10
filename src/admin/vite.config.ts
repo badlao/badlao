7ff90b98332ff97c918a196a8fca90f629b0407a
root <root@vps-269fed1e.vps.ovh.us>
2026-03-10 09:15:17 +0000

    vite allow

---
+++ src/admin/vite.config.ts
@@ -0,0 +1,10 @@
+import { mergeConfig, type UserConfig } from 'vite';
+
+export default (config: UserConfig) => {
+  return mergeConfig(config, {
+    server: {
+      host: '0.0.0.0',
+      allowedHosts: true,
+    },
+  });
+};
